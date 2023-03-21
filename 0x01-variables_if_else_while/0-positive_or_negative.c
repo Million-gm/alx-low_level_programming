39be9737f64f126d6ffcb356083cf5aacf25266d
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-22 00:19:38 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,8 +15,8 @@ int main(void) {
   if (n == 0)
     printf("%d is zero \n", n);
   else if (n > 0)
-    printf("%d is postive \n", n);
+    printf("%d is positive\n", n);
   else
-    printf("%d is negative \n", n);
+    printf("%d is negative\n", n);
   return (0);
 }
