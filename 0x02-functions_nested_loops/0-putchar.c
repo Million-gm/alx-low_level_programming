0bf7820225a147f9c65d20f45329240a25e4002d
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-22 15:14:26 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+#include<main.h>
 
+/**
+*main: main function
+*RETURN:int 0
+*/
+int main(void)
+{
+ 
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+return (0);
+}
