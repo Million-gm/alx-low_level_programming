9ae6709d023c61db3d1809b9a1ea714ec208cfa3
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-22 21:46:48 +0300

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_abs(int);- absolute value of a num
+*@c: num to change on absolute
+*Return: absolute value of c
+*/
+
+int _abs(int c)
+{
+if (c < 0)
+return (c * (-1));
+else
+return (c);
+}
