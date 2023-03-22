cd09b9fd4889d0bbf49fc4eb1f95cd2287b882ee
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-22 21:13:13 +0300

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_islower- checks if character is lowercase
+*@c: character ascii no to be checked
+*Return: 0 if uppercase, 1 if lowercase
+*/
+
+int _isalpha(int c)
+{
+if (c >= 65 && c <= 122)
+return (1);
+else
+return (0);
+}
