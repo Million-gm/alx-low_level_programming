eddd680ded41940b9cf5e15432b9f6f40cba3801
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-23 00:58:12 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,24 +9,24 @@ void print_to_98(int n)
 {
 if (n == 98)
 {
-printf("%d",n);
+printf("%d", n);
 }
 else if (n > 98)
 {
-while (n >=98)
+while (n >= 98)
 {
-printf("%d",n);
-if (n !=98)
+printf("%d", n);
+if (n != 98)
 printf(", ");
 n--;
 }
 }
 else
 {
-while (n <=98)
+while (n <= 98)
 {
-printf("%d",n);
-if (n !=98)
+printf("%d", n);
+if (n != 98)
 printf(", ");
 n++;
 }
