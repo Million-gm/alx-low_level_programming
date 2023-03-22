38a74bd13f5281d4cd78818571b0747dda5b09ab
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-22 21:26:39 +0300

    Create 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_sign(int n)- checks if characters is sign
+*@c: character ascii no to be checked
+*Return: 0 if 0, 1 if greater than zero, -1 if less than zero
+*/
+
+int print_sign(int n)
+{
+if (c > 0)
+return (1);
+ if (c < 0)
+return (-1);
+else 
+return (0);
+}
