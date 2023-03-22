67fbcfd5a8fceaf2955ff436133fb1a5158b0d5c
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-22 21:57:09 +0300

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*print_last_digit- prints last digit
+*@c: character ascii no to be checked
+*Return:last digit of c
+*/
+
+int print_last_digit(int c)
+{
+return (c % 10);
+}
