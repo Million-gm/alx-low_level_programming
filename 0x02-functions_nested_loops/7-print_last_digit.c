fd57d5d4d1b1005bfa60352e0acc5a1e990c4958
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-22 22:33:10 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include<limits.h>
 /**
 *print_last_digit- prints last digit
 *@c: character ascii no to be checked
@@ -9,6 +10,12 @@ int print_last_digit(int c)
 {
 if (c < 0)
 c = c * -1;
+if (c==INT_MIN)
+{
+c= -( c % 10);
+_putchar(c + '0');
+return (c);
+}
 c = c % 10;
 _putchar('0' + c);
 return (c);
