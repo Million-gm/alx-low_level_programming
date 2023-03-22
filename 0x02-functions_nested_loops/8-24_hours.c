79be0fc1dfff3a48036fbf9beff15cd9061aeb46
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-22 23:27:09 +0300

    Create 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+int c, m;
+for (c - 0; c < 24; c++)
+{
+for (m = 0; m< 60;m++)
+{
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+_putchar(':');
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+_putchar('\n');
+}
+}
