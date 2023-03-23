586af1fab6e0345b7d3963e7d11d228489e0d0c1
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-23 03:43:28 +0300

    Create 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - prints time table
+ *@n: nuber of time table range
+ */
+void print_times_table(int n)
+{
+int c, m, r;
+for (c = 0; c <= n; c++)
+{
+for (m = 0; m <= 10; m++)
+{
+r = c * m;
+if (m == 0)
+_putchar('0' + r);
+if (r < 10 && m != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + r);
+}
+else if (r >= 10 && r < 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + (r / 10));
+_putchar('0' + (r % 10));
+}
+else if(r >= 100)
+{
+int mid;
+mid%=10;
+mid/=10;
+mid%=10;
+_putchar(',');
+_putchar(' ');
+_putchar('0' + (r / 10));
+_putchar('0' + mid);
+_putchar('0' + (r % 10));
+}
+}
+_putchar('\n');
+}
+}
