42349115fdba152ed733f88e0d70248d4c4e28af
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-24 01:50:34 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,11 +10,15 @@ void print_diagonal(int n)
 if (n != 0)
 {
 int i;
-for (i = n; i > 0; i--)
+int j;
+for (j = n; j > 0; j--)
+{
+for (i = 0; i < n; i--)
 {
 _putchar(' '); 
 }  
 _putchar('\\');
 }
 _putchar('\n');
+}
 }
