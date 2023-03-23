45810b9c37974114304d747a166e89957b7ebc86
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-23 23:25:10 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,11 +3,12 @@
 *print_numbers- prints 0 to 9
 *Return: void
 */
-void print_numbers()
+void print_numbers(void)
 {
 int i;
 for (i = 0; i <= 9; i++)
 {
-_putchar('0'+i);
+_putchar('0' + i);
 }
+_putchar('\n');
 }
