b8220ad09630c28dea2264c64c8ce0c09f84e36f
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-23 03:51:18 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * times_table - prints time table
+ * print_times_table - prints time table
  *@n: nuber of time table range
  */
 void print_times_table(int n)
@@ -9,7 +9,7 @@ void print_times_table(int n)
 int c, m, r;
 for (c = 0; c <= n; c++)
 {
-for (m = 0; m <= 10; m++)
+for (m = 0; m <= n; m++)
 {
 r = c * m;
 if (m == 0)
@@ -33,9 +33,9 @@ _putchar('0' + (r % 10));
 else if(r >= 100)
 {
 int mid;
-mid%=10;
-mid/=10;
-mid%=10;
+mid %= 10;
+mid /= 10;
+mid %= 10;
 _putchar(',');
 _putchar(' ');
 _putchar('0' + (r / 10));
