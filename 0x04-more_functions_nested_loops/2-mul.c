26d0fa61e92b1e814e94d9ecdbbfde2f4bdd2690
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-23 22:16:25 +0300

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*mul- cmultiplies two digits
+*@a: character code to be checked
+*@b: character code to be checked
+*Return: 1 if digit, 0 if no digit
+*/
+int mul(int a, int b)
+{
+int mul  = a * b;
+return mul;
+}
