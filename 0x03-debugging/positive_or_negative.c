478b5ce1f36b0a82f9a284c8401aa30649088a90
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-25 01:37:49 +0300

    Update positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -9,11 +9,11 @@
 */
 void positive_or_negative(int i)
 {
-if (n == 0)
-printf("%d is zero\n", n);
-else if (n > 0)
-printf("%d is positive\n", n);
+if (i == 0)
+printf("%d is zero\n", i);
+else if (i > 0)
+printf("%d is positive\n", i);
 else
-printf("%d is negative\n", n);
+printf("%d is negative\n", i);
 return (0);
 }
