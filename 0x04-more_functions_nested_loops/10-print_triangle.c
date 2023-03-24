e05bc2433940478a68b454c811c8c90eeb0f1a04
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-24 03:42:02 +0300

    Create 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,54 @@
+#include "main.h"
+void printhash(int r);
+void printsp(int r);
+/**
+*print_triangle - prints 0 to 9
+*@size: size of the triangle
+*Return: void
+*/
+void print_triangle(int size)
+{
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i, j;
+for (i = size; i > 0; i--)
+{
+for (j = 1; j <= size; j++)
+{
+printsp((size - j));
+printhash(j);
+}
+  
+_putchar('\n');
+}
+}
+}
+/**
+*printsp - prints 0 to 9
+*@r: number of space
+*Return: void
+*/
+void printsp(int r)
+{
+while (r > 0)
+{
+_putchar(' ');
+r--;
+}
+/**
+*printhash - prints 0 to 9
+*@r: number of #
+*Return: void
+*/  
+void printhash(int r)
+{
+while (r > 0)
+{
+_putchar('#');
+r--;
+}
+}
