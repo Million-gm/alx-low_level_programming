04d8c3aff5a213394e44ab1e64a89d2cc58351c3
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-24 04:29:04 +0300

    Create 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <math.h>
+#include<stdio.h>
+/**
+*main- prime factor
+*Return: 0
+*/
+int main(void)
+{
+long i, k, c;
+k=0;
+i=612852475143;
+while (i % 2 ==0)
+{
+i /= 2;
+k = 2; 
+}
+for (c = 0; c <= sqrt(i); c += 2)
+{
+while (i % c == 0)
+{
+if (k < i)
+k=i;
+i /= c;
+}
+}
+printf("%d", k)
+return (0);
+}
