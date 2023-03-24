2c4277fb225ff87a3cb0bb70836e23127095cc71
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-24 05:18:53 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,21 +7,18 @@ int main(void)
 {
 long double i, k, c;
 k = 0;
-printf("%ld", k);
 i = 612852475143;
 while ((i % 2) == 0)
 {
 i = i / 2;
 k = 2;
 }
-printf("%ld", k);
 for (c = 0; c <= sqrt(i); c += 2)
 {
 while ((i % c) == 0)
 {
 if (k < i)
 k = i;
-printf("%ld", k);
 i = i / c;
 }
 }
