e62780a87e4794ec926c23a7b054bd75985bc5d3
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-26 02:58:35 +0300

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+*main- main function
+*Return: 0
+*/
+int main(void)
+{
+int n;
+
+for (n = 97; n <= 122; n++)
+{
+putchar('0' + n);
+}
+putchar('\n');
+	return (0);
+}
