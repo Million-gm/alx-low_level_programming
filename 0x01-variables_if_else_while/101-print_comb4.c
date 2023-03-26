2bf14bf2a6861af8e0632043741e41e06afd975e
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-26 03:42:51 +0300

    Create 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+*main- main function
+*Return: 0
+*/
+int main(void)
+{
+int n, i, j;
+
+for (n = 48; n <= 50; n++)
+{
+for (i = n + 1; i <= 56; n++)
+{
+for (j = i + 1; j <= 57; n++)
+{
+putchar(n);
+putchar(i);
+putchar(j);
+putchar(',');
+putchar(" ");
+
+}
+}
+}
+putchar('\n');
+	return (0);
+}
