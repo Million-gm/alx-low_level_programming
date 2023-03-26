8807e95cdf7219d9d2caf5ae00cdf96996469ee3
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-26 03:17:37 +0300

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+*main- main function
+*Return: 0
+*/
+int main(void)
+{
+int n;
+
+for (n = 122; n <= 97; n--)
+{
+putchar(n);
+}
+putchar('\n');
+	return (0);
+}
