6445ecb06862e94d9635e0fe055a62df688c5cd3
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-26 03:09:54 +0300

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+*main- main function
+*Return: 0
+*/
+int main(void)
+{
+int n;
+
+for (n = 97; n <= 122; n++)
+{
+if (n == 101 || n = 113)
+continue;
+putchar(n);
+}
+putchar('\n');
+	return (0);
+}
