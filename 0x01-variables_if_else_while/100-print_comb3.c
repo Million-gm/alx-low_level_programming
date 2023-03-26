957954ef1936e3ac4c1d909f699e6bd94c2fe852
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-26 03:56:31 +0300

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+*main- main function
+*Return: 0
+*/
+int main(void)
+{
+int i, j;
+
+
+for (i = 48; i <= 56; i++)
+{
+for (j = i + 1; j <= 57; j++)
+{
+putchar(i);
+putchar(j);
+putchar(',');
+putchar(' ');
+
+
+}
+}
+putchar('\n');
+	return (0);
+}
