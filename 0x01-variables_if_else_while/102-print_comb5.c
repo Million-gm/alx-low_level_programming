52559be7ae43d5df76d66202574f294936d5b7ed
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-26 04:31:58 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,13 +13,11 @@ for (n = 48; n <= 57; n++)
 {
 for (i = 48; i <= 57; i++)
 {
-for (k = 48; k <= 56; k++)
+for (k = 48; k <= 57; k++)
 {
-if (i > 48)
-j = i + 1;
-else
-j = i;
-while (j <= 57)
+if (i =48 && n =48 && k =48)
+{
+for (j = i; j <= 57; j++)
 {
 putchar(n);
 putchar(i);
@@ -31,7 +29,6 @@ if (n < 57)
 putchar(',');
 putchar(' ');
 }
-j++;
 }
 }
 }
