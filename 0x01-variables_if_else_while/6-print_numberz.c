33a778853da9ed2b4b349f9ed138bffbc0c73ffd
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-26 03:16:03 +0300

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+*main- main function
+*Return: 0
+*/
+int main(void)
+{
+int n;
+
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+putchar('\n');
+	return (0);
+}
