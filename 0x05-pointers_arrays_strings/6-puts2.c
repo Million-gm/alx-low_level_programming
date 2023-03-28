1c76ec6aca92ff6518e257e78a4058c0dda1b60c
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-28 19:04:52 +0300

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*puts2- reverse the string
+*@str: the string
+*/
+void puts2(char *str)
+{
+int i = 0;
+while (*(str + i) != '\0')
+{
+if ((i % 2) == 0)
+printf("%c", *(str + i));
+i++;
+}
+printf("\n");
+}
