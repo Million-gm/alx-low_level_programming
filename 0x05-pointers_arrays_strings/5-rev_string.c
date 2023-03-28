9acea5ebca96bb838c1ea379749ddfb69960ddda
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-28 18:46:14 +0300

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*rev_string- reverse the string
+*@s: the string
+*/
+void rev_string(char *s
+{
+int i = 0;
+while (*(s + i) != '\0')
+{
+i++;
+}
+i--;
+int j = i + 1;
+char c[j];
+while (i >= 0)
+{
+c[j] = *(s + i);
+j++;
+i--;
+}
+*s = c;
+}
