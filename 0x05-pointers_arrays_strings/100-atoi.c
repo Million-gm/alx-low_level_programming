045c4f2651dfe58fee26981be672a02bd648146b
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-28 21:52:46 +0300

    Create 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+*_atoi- convert a string to an integer
+*@s: the string
+*Return: the integer value of s
+*/
+int _atoi(char *s)
+{
+	int i;
+	int m, p;
+i = 0;
+	m = 0;
+	p = -1;
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			p *= -1;
+
+		if (s[i] > 47 && s[i] < 58)
+		{
+			if (h < 0)
+				m = (m * 10) - (s[i] - '0');
+			else
+				m = (s[i] - '0') * -1;
+
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+				break;
+		}
+ i++;
+	}
+	if (p < 0)
+		m =m * -1;
+
+	return (h);
+}
