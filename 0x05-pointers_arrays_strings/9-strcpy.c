faa5e77e90a531e1fd961b1e31f8b1dda7e8e84e
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-28 21:09:03 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,11 +6,12 @@
 *@dest: the string destination
 *return: the characthers in dest
 */
-char *_strcpy(char *dest, char *src){
+char *_strcpy(char *dest, char *src)
+{
 int i = 0;
 do
 {
-*(dest + i) = *(src + i);
+dest[i] = src[i];
 i++;
 }while (*(src + i) != '\0');
 return (dest);
