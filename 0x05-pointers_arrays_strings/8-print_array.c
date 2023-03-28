210e072739e84ffce4d3118a9de7635999c83ac1
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-28 21:34:15 +0300

    Create 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<stdio.h>
+/**
+*print_array-prints the array data
+*@a: the string
+*@n: the string
+*/
+void print_array(int *a, int n)
+{
+int i = 0;
+while (i < n)
+{
+printf("%d", *(a + i));
+i++;
+}
+printf("\n");
+}
