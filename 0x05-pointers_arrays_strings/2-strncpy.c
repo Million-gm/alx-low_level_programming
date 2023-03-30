89967b3baf28a367440994ee9aa840f1832a1e48
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-30 06:11:33 +0300

    Create 2-strncpy.c

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+*_strncat- concatinates two strings
+*@dest: the concatinated string destination
+*@src: the source string for concatination
+*@n: bytes from src
+*Return: concatinated string dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0;
+int j = 0;
+while (*(src + n) == '\0')
+{
+if (n <= 0)
+break;
+n--;
+}
+while (*(dest + i) != '\0')
+{
+i++;
+}
+while (j < n && *(src + j))
+{
+*(dest + j) = *(src + j);
+j++;
+i++;
+}
+for (i = j; i < n; i++)
+		dest[i] = '\0';
+
+return (dest);
+}
