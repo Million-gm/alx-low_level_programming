9b1d89a2b3fd3a802327b4007ee3adb46c39db13
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-30 07:22:53 +0300

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*_strcmp- copies src to dest
+*@s1: the first string to compare
+*@s2: the second string to compare
+*Return: difference value of the strings
+*/
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+int j = 0;
+int k = n;
+while (*(s1 + i) == '\0' && *(s2 + i) == '\0')
+{
+j = *(s1 + i) - *(s2 + i);
+i++;
+if (j != 0)
+break;
+}
+return (j);
+}
