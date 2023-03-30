b4ef0e89215f45678e4c4fbf0b368c363d072050
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-30 07:47:18 +0300

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+*reverse_array- reverses array
+*@a: the array
+*@src: the source string for concatination
+*@n: array size
+*/
+void reverse_array(int *a, int n)
+{
+int i = 0;
+int k = n;
+while (*(a + i) != 0)
+{
+*(a + k) = *(a + i);
+k++;
+i++;
+}
+k = n;
+while (i >= 0)
+{
+*(a + i) = *(a + k);
+k++;
+i--;
+}
+while (k >= n)
+{
+*(a + k) = '\0';
+k--;
+}
+return (dest);
+}
