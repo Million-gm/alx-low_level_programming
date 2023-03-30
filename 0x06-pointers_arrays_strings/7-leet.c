22f80a6a63f466bf2a3e670e137de35c405f236c
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-30 20:43:18 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,18 +7,19 @@
 char *leet(char *c)
 {
 int i = 0;
+int m;
+char s[] = {"AEOTLaeotl"};
+char sn[] = {"4307143071"};
 while (*(c + i) != '\0')
 {
-if (*(c + i) == 'a'|| *(c + i) == 'A')
-*(c + i) = '4';
-else if (*(c + i) == 'e'|| *(c + i) == 'E')
-*(c + i) = '3';
-else if (*(c + i) == 'o'|| *(c + i) == 'O')
-*(c + i) = '0';
-else if (*(c + i) == 't'|| *(c + i) == 'T')
-*(c + i) = '7';
-else if (*(c + i) == 'l'|| *(c + i) == 'L')
-*(c + i) = '1';
+m = 0;
+while (m < 10)
+{
+if (*(c + i) == s[m])
+{
+*(c + i) = sn[m]
+}
+}
 i++;
 }
 
