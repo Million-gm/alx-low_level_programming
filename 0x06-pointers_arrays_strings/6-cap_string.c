089dc16bdef1a170770d50a779984629eaddaba8
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-30 20:06:49 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-*string_toupper- copies src to dest
+*cap_string- capitalizes every word
 *@c: the letters
 *Return: uppercase of string c
 */
-char *string_toupper(char *c)
+char *cap_string(char *c)
 {
 int i = 0;
 while (*(c + i) != '\0')
@@ -13,6 +13,10 @@ if (*(c + i) == ' ')
 {
 if (*(c + i + 1) >= 97 && *(c + i + 1) <= 122 && *(c + i + 1) != '\0')
 *(c + i + 1) = *(c + i + 1) - 32;
+}
+else if (i == 0 && *(c + i) >= 97 && *(c + i ) <= 122)
+{
+*(c + i) = *(c + i) - 32;
 }
 i++;
 }
