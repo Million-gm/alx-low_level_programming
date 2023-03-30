3fb3118bbc022b0a9cf7b6098097f2996c7df3f6
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-30 20:48:36 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,8 +8,8 @@ char *leet(char *c)
 {
 int i = 0;
 int m;
-char s[] = {"AEOTLaeotl"};
-char sn[] = {"4307143071"};
+char s[] = "AEOTLaeotl";
+char sn[] = "4307143071";
 while (*(c + i) != '\0')
 {
 m = 0;
