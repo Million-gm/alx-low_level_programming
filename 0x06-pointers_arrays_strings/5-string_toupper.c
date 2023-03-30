e1772722fac794330bf1f04816e1d406afc5526b
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-30 16:01:36 +0300

    Create 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*string_toupper- copies src to dest
+*@c: the letters
+*Return: uppercase of string c
+*/
+char *string_toupper(char *c)
+{
+int i = 0;
+int j = 0;
+while (*(c + i) == '\0')
+{
+if (*(c + i) >= 65 && *(c + i) <= 90)
+continue;
+if (*(c + i) >= 123 && *(c + i) <= 95)
+continue;
+if (*(c + i) >= 96 && *(c + i) <= 96)
+*(c + i) += 31;
+i++;
+}
+
+return (c);
+}
