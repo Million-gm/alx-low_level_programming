250c807d26d39a0c4bc458a7a933ba9f83d79559
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-30 18:51:04 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,10 +9,6 @@ char *string_toupper(char *c)
 int i = 0;
 while (*(c + i) != '\0')
 {
-if (*(c + i) >= 65 && *(c + i) <= 90)
-continue;
-if (*(c + i) >= 123 && *(c + i) <= 96)
-continue;
 if (*(c + i) >= 97 && *(c + i) <= 122)
 *(c + i) = *(c + i) - 32;
 i++;
