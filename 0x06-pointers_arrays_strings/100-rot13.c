e0a4fa6ebdf65e7cb7c9ab39bfe5fb6b5124d31c
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-30 21:53:27 +0300

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,32 +1,29 @@
 #include "main.h"
 /**
- * rot13 - encodes a string using rot13
+ * rot13 - rotates a string by 13
  * @str: the string to encode
  *
  * Return: encode string
  */
 char *rot13(char *str)
 {
-	int i;
-  int m;
-
-	char undecoded[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char encoded[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		j = 0;
-		while (undecoded[m] != '\0')
-		{
-			if (str[i] == undecoded[m])
-			{
-				str[i] = encoded[m];
-				break;
-			}
-			m++;
-		}
-		i++;
-	}
-	return (str);
+int i = 0;
+int m;
+char undecoded[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char encoded[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+while (str[i] != '\0')
+{
+m = 0;
+while (undecoded[m] != '\0')
+{
+if (str[i] == undecoded[m])
+{
+str[i] = encoded[m];
+break;
+}
+m++;
+}
+i++;
+}
+return (str);
 }
