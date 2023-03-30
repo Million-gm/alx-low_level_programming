a6242f46579e0a15f486651cbe8faac2564a2acf
Million-gm <67041017+Million-gm@users.noreply.github.com>
2023-03-30 13:38:17 +0300

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,6 @@
 #include "main.h"
-#include <stdio.h>
 /**
-*reverse_array- reverses array
+*reverse_array- reverses the array
 *@a: the array
 *@n: array size
 */
@@ -21,9 +20,7 @@ k = n + 1;
 while (i >= 0)
 {
 *(a + i) = *(a + k);
-printf("the counters are \ti=%d\tk=%d\n", i, k);
 k++;
 i--;
 }
-printf("the counters at last are \ti=%d\tk=%d\n", i, k);
 }
